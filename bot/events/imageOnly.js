2afd2cb3b6741f042beeb8542066cffe95a3edb4
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-26 14:34:12 -0500

    image only

--- bot/events/imageOnly.js
+++ bot/events/imageOnly.js
@@ -4,6 +4,12 @@ import { Client } from 'discord.js'
  * 
  * @param {Client} client
  */
-export default function({client}){
-    console.log("hello")
+export default function(client){
+    client.on("messageCreate", (msg)=>{
+        if(msg.author.bot==true)return;
+
+        if(msg.channel.name!="cheese")return;
+        if(msg.attachments.size>0)return;
+        msg.delete()
+    })
 }
