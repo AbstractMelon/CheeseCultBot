a22b939aff2ab37e1b52947264007c88854f3143
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-26 15:30:50 -0500

    less duplicates

--- bot/commands/randomCheese.js
+++ bot/commands/randomCheese.js
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder, CommandInteraction, Client } from 'discord.js'
 import {lazyEmbed} from '../lazyEmbed.js';
 
+const pastImages = []
+
 export default  {
 	data: new SlashCommandBuilder()
 		.setName('random-cheese')
@@ -11,7 +13,13 @@ export default  {
          * @param {Client} client
          */
 	async execute(interaction, client) {
-        let cheeseImage = (await(await fetch("https://cheese-thing.vercel.app/api")).json()).image
+        let cheeseImage = undefined
+        while(cheeseImage==undefined || pastImages.includes(cheeseImage)){
+            cheeseImage = (await(await fetch("https://cheese-thing.vercel.app/api")).json()).image
+        }
+        pastImages.push(cheeseImage)
+        if(pastImages.length>10)pastImages.pop()
+
         await interaction.reply({content:cheeseImage,
             ephemeral:!(interaction.channel.name.includes("command")||interaction.channel.name.includes("bot")||interaction.channel.name=="cheese")});
 	},
