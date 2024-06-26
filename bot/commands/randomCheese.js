ca03cc9f93f065f5c6ad96a35527ac78a4fb2389
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-26 15:26:51 -0500

    random cheese command

---
+++ bot/commands/randomCheese.js
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder, CommandInteraction, Client } from 'discord.js'
+import {lazyEmbed} from '../lazyEmbed.js';
+
+export default  {
+	data: new SlashCommandBuilder()
+		.setName('random-cheese')
+		.setDescription('Replies with random cheese image'),
+        /**
+         * 
+         * @param {CommandInteraction} interaction 
+         * @param {Client} client
+         */
+	async execute(interaction, client) {
+        let cheeseImage = (await(await fetch("https://cheese-thing.vercel.app/api")).json()).image
+        await interaction.reply({content:cheeseImage,
+            ephemeral:!(interaction.channel.name.includes("command")||interaction.channel.name.includes("bot"))});
+	},
+};
